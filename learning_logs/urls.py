70436ecf6d0f08921c699fd5cc8b30bfb45673f1
Binod-Bk <gautambinod115@gmail.com>
2025-01-02 01:13:26 -0500

    Adding main page/multiple pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     #Home Page
     path('',views.index, name='index'),
+    path('',views.about_us, name='about_us'),
+    path('topics/',views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     """The home page for learning log."""
     return render(request, 'learning_logs/index.html')
+
+def about_us(request):
+    """The about us page for learning log."""
+    return render(request, 'learning_logs/about_us.html')
+
+def topics(request):
+    """Show all Topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
