d684e3fef998f68916e33630a797a4c0e46a3ca1
Binod-Bk <gautambinod115@gmail.com>
2025-01-09 10:52:07 -0500

    Adding new topics using Forms

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,10 +5,12 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    #Home Page
+    # Home Page
     path('',views.index, name='index'),
     path('',views.about_us, name='about_us'),
     path('topics/',views.topics, name='topics'),
-    #Detail page for a single topic. (id)
+    # Detail page for a single topic. (id)
     path('topics/<int:topic_id>/',views.topic, name='topic'),
+    # Page for adding a new Topic using a form.
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
