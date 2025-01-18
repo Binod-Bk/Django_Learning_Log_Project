70d1d9f8f55f532b58591908d76c3259a20de3c0
Binod-Bk <gautambinod115@gmail.com>
2025-01-18 13:53:46 -0500

    Editing Entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,13 +6,15 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Home Page
-    path('',views.index, name='index'),
-    path('',views.about_us, name='about_us'),
-    path('topics/',views.topics, name='topics'),
+    path('', views.index, name='index'),
+    path('', views.about_us, name='about_us'),
+    path('topics/', views.topics, name='topics'),
     # Detail page for a single topic. (id)
-    path('topics/<int:topic_id>/',views.topic, name='topic'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
     # Page for adding a new Topic using a form.
     path('new_topic/', views.new_topic, name='new_topic'),
     # Page for adding a new Entry using a form.
-    path('new_entry/<int:topic_id>/',views.new_entry, name='new_entry'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editing an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
