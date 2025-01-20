9e6e357612a6b795faebea8fd3a9e2cddcc7bbab
Binod-Bk <gautambinod115@gmail.com>
2025-01-20 09:22:51 -0500

    Restricting unauthorized access

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -20,6 +20,7 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries. """
     topic = Topic.objects.get(id=topic_id)
@@ -27,6 +28,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html',context)
 
+@login_required
 def new_topic(request):
     """Adding a new topic."""
     if request.method != 'POST':
@@ -43,7 +45,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
     """Adding a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -64,6 +66,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry. """
     entry = Entry.objects.get(id=entry_id)
